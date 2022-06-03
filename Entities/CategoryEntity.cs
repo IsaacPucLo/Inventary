89c2dd1d24379eecd115783e1a1c12b4a16b305c
Isaac Puc <dual.isaac.puc@gmail.com>
2022-06-03 15:45:01 -0500

    Creacion de entidades: se creó un nuevo poryecto dentro de la solucion de Biblioteca de clases en la cual almacenaremos nuestras clases y creamos la entidad Category

---
+++ Entities/CategoryEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities {
+    public class CategoryEntity {       //Agruegar public sino aparece en un principio
+        
+        [Key]
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
