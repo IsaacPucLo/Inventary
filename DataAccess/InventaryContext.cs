d936054f64bb8380665998bbd9566e87426d11fb
Isaac Puc <dual.isaac.puc@gmail.com>
2022-06-05 22:41:51 -0500

    Se alimentó a la base de datos, se cambió la linea de conexión a la base de datos y se generaron datos aleatorios, se craron nuevas migraciones con la información previa y se utilizó el update-database para crear la base de datos en SQLServer

--- DataAccess/InventaryContext.cs
+++ DataAccess/InventaryContext.cs
@@ -17,8 +17,29 @@ namespace DataAccess {
 
         protected override void OnConfiguring(DbContextOptionsBuilder options) {
             if (!options.IsConfigured) {
-                options.UseSqlServer("Server=USERP-C; Database=InventarDb; User Id=USER-PC; Password=");
+                //options.UseSqlServer("Server=USER-PC; Database=InventaryDb; User Id=User-PC\\User; Password=");
+                
+                //Usé esta linea de codigo debido a que mi base de datos no tiene contraseña
+                options.UseSqlServer("Server=USER-PC ;Database=InventaryDB; Trusted_Connection=True;");
             }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CategoryEntity>().HasData(
+                new CategoryEntity { CategoryId = "ASH", CategoryName = "Aseo Hogar" },
+                new CategoryEntity { CategoryId = "ASP", CategoryName = "Aseo Personal" },
+                new CategoryEntity { CategoryId = "HGR", CategoryName = "Hogar" },
+                new CategoryEntity { CategoryId = "PRF", CategoryName = "Perfumería" },
+                new CategoryEntity { CategoryId = "SLD", CategoryName = "Salud" },
+                new CategoryEntity { CategoryId = "VDJ", CategoryName = "Video Juegos" }
+            );
+
+            modelBuilder.Entity<WarehouseEntity>().HasData(
+                new WarehouseEntity { WarehouseId = new Random().Next(0, 999), WarehouseName = "Bodega Central", WarehouseAddress = "Calle 8 con 23" },
+                new WarehouseEntity { WarehouseId = new Random().Next(0,999), WarehouseName = "Bodega Norte", WarehouseAddress = "Calle norte con occidente" }
+            );
         }
     }
 }
---
+++ DataAccess/Migrations/20220606033656_AddData.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class AddData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { "ASH", "Aseo Hogar" },
+                    { "ASP", "Aseo Personal" },
+                    { "HGR", "Hogar" },
+                    { "PRF", "Perfumería" },
+                    { "SLD", "Salud" },
+                    { "VDJ", "Video Juegos" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Warehouses",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[,]
+                {
+                    { 249, "Calle 8 con 23", "Bodega Central" },
+                    { 706, "Calle norte con occidente", "Bodega Norte" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "ASH");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "ASP");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "HGR");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "PRF");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "SLD");
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: "VDJ");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: 249);
+
+            migrationBuilder.DeleteData(
+                table: "Warehouses",
+                keyColumn: "WarehouseId",
+                keyValue: 706);
+        }
+    }
+}
